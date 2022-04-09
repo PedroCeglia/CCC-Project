90218bdfe534520f09f6d871437a4a449556e2dc
PedroCeglia <ceglia.pedro@gmail.com>
2022-04-09 11:52:25 -0300

    Add Login and AreaSocio Routes

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -3,12 +3,16 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 // Import Pages
 import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import AreaSocio from '../Pages/AreaSocio'
 
 export default function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/area-socio" element={<AreaSocio/>}/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     )
