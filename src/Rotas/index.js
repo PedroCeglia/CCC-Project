6d93ea6233e6faca158d552bc370403889910c69
PedroCeglia <ceglia.pedro@gmail.com>
2022-04-11 19:02:50 -0300

    Add CreateUser Route

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -5,14 +5,19 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import AreaSocio from '../Pages/AreaSocio'
+import CreateUser from '../Pages/CreateUser'
 
 export default function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/area-socio" element={<AreaSocio/>}/>
-                <Route path="/login" element={<Login/>}/>
+                <Route path='/'>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/area-socio" element={<AreaSocio/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/create-user" element={<CreateUser/>}/>                    
+                </Route>
+
             </Routes>
         </BrowserRouter>
     )
