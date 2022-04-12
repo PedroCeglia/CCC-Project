40321332bc69644d653ec78c805d4ba7526e33ba
PedroCeglia <ceglia.pedro@gmail.com>
2022-04-12 12:40:58 -0300

    Pass User to ALL Routes

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+// Import Auth API Functions
 
 // Import Pages
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import AreaSocio from '../Pages/AreaSocio'
 import CreateUser from '../Pages/CreateUser'
+import { verifyUserLog } from '../Firebase/API/Auth'
 
 export default function Rotas(){
+
+    const [user, setUser] = useState(null)
+    useEffect(()=>{
+        verifyUserLog(setUser)
+        console.log(user)
+    },[])
+
+
     return(
         <BrowserRouter>
             <Routes>
                 <Route path='/'>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/area-socio" element={<AreaSocio/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/create-user" element={<CreateUser/>}/>                    
+                    <Route path="/" element={<Home user={user}/>}/>
+                    <Route path="/area-socio" element={<AreaSocio user={user}/>}/>
+                    <Route path="/login" element={<Login user={user}/>}/>
+                    <Route path="/create-user" element={<CreateUser user={user}/>}/>                    
                 </Route>
 
             </Routes>
