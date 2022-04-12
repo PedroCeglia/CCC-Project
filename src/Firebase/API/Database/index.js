50fa76a7910faca022f7938754114537779f5181
PedroCeglia <ceglia.pedro@gmail.com>
2022-04-12 12:48:37 -0300

    Add createUserInDatabase and setUserNameInDatabase

--- src/Firebase/API/Database/index.js
+++ src/Firebase/API/Database/index.js
@@ -2,4 +2,17 @@
 import { database } from "../..";
 
 // Import DatabaseAPI Functions
-import {} from "firebase/database"
+import {ref, update} from "firebase/database"
+
+export function createUserInDatabase(name, email, id){
+    update(ref(database,`user/${id}`),{
+        id:id,
+        name:name,
+        email:email
+    })
+}
+export function setUserNameInDatabase(name, id){
+    update(ref(database,`user/${id}`),{
+        name:name
+    })
+}
